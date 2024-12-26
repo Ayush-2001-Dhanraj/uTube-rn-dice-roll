479ac6158fe790b7371a0ab3cccd7d7e9faa9812
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-12-26 23:04:21 +0530

    random Texts

--- App.tsx
+++ App.tsx
@@ -30,25 +30,74 @@ type DiceProps = {
   imageSource: ImageSourcePropType;
 };
 
+interface DimInterface {
+  height: number;
+  width: number;
+}
+
 const Dice = ({imageSource}: DiceProps): JSX.Element => {
   return <Image source={imageSource} style={styles.dice} />;
 };
 
 const App = () => {
   const [currentFace, setCurrentFace] = useState<ImageSourcePropType>(Dice1);
+  const [containerDim, setContainerDim] = useState<DimInterface>({
+    height: 0,
+    width: 0,
+  });
+  const [currentTexts, setCurrentTexts] = useState<Array<JSX.Element>>([]);
+
+  const getTextComponent = (num: number) => {
+    let color = '#';
+    const choices = '0123456789ABCDEF';
+    for (let i = 0; i < 6; i++)
+      color += choices[Math.floor(Math.random() * choices.length)];
+    const fontSize = Math.floor(Math.random() * 45) + 20;
+    const top = Math.random() * (containerDim.height - fontSize);
+    const left = Math.random() * (containerDim.width - fontSize);
+
+    return (
+      <Text
+        style={[
+          styles.randomTxt,
+          {
+            fontSize,
+            color,
+            left,
+            top,
+          },
+        ]}>
+        {num}
+      </Text>
+    );
+  };
+
+  const addNumberRandomly = (num: number) => {
+    const currText = getTextComponent(num);
+    setCurrentTexts(preV => [...preV, currText]);
+  };
 
   const handleDiceRoll = () => {
     const choice = Math.floor(Math.random() * 6);
     setCurrentFace(diceImages[choice]);
     trigger('impactLight');
+    addNumberRandomly(choice + 1);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+      style={styles.container}
+      onLayout={event => {
+        const {height, width} = event.nativeEvent.layout;
+        setContainerDim({height, width});
+      }}>
       <Dice imageSource={currentFace} />
       <TouchableOpacity style={styles.btn} onPress={handleDiceRoll}>
         <Text style={styles.rollTxt}>Roll</Text>
       </TouchableOpacity>
+      {currentTexts.map((Comp, i) => {
+        return React.cloneElement(Comp, {key: i});
+      })}
     </SafeAreaView>
   );
 };
@@ -60,13 +109,14 @@ const styles = StyleSheet.create({
     height: 150,
     width: 150,
     borderRadius: 10,
+    zIndex: 100,
   },
   container: {
     flex: 1,
-    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
     gap: 10,
+    position: 'relative',
   },
   rollTxt: {
     fontWeight: 'bold',
@@ -79,5 +129,9 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     borderRadius: 8,
     elevation: 8,
+    zIndex: 100,
+  },
+  randomTxt: {
+    position: 'absolute',
   },
 });
